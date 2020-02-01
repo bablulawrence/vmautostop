cf8afb6039be13bec0acad56dfe8eccc2326140a
Bablu Lawrence <bablulawrence@outlook.com>
2020-02-01 03:59:31 +0000

    Added .funcignore and updated .gitignore

--- azure/cmd-az-functions.sh
+++ azure/cmd-az-functions.sh
@@ -1,14 +1,24 @@
 mkdir vmautostop
 cd vmautostop
 mkdir azure
-python3.7 venv env
+python3.7 -m venv env
 pip install -r requirements.txt
 az group create --name rg-vmautostop -location southeastasia
 az storage account create --name stgvmautostop --resource-group rg-vmautostop
-az functionapp create --name fn-vmautostop --resource-group rg-vmautostop --consumption-plan-location southeastasia 
-    --os-type Linux --runtime python --runtime-version 3.7 --storage-account stgvmautostop
+
+az functionapp create --name fn-vmautostop --resource-group rg-vmautostop \
+    --consumption-plan-location southeastasia \
+    --os-type Linux --runtime python --runtime-version 3.7 \
+    --storage-account stgvmautostop
+
+az functionapp config appsettings list --name fn-vmautostop \
+    --resource-group rg-vmautostop 
+
 az functionapp config appsettings set --name fn-vmautostop \
-	--resource-group rg-vm-autostop --settings FUNCTIONS_EXTENSION_VERSION=~3.0
+    --resource-group rg-vmautostop \
+	--settings FUNCTIONS_EXTENSION_VERSION=~3
+
+az functionapp delete --name fn-vmautostop --resource-group rg-vmautostop
 
 func init fn-vmautostop --worker-runtime python
 cd fn-vmautostop
